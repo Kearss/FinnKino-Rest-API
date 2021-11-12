578e56bf348c5d2f4c16d1a2cfa00b334fc45c6e
Kearsz <16836684+Kearsz@users.noreply.github.com>
2021-11-12 12:34:59 +0200

    html valmis  mutta ei js
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- FinnKino.js
+++ FinnKino.js
@@ -1,1 +1,6 @@
-
+function loadByTheatre(){
+    var url = "https://www.finnkino.fi/xml/Schedule/";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
