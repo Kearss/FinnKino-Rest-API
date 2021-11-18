757f78b6b67b30c27e8910fd68a0e149a46eb277
Kearsz <16836684+Kearsz@users.noreply.github.com>
2021-11-18 15:51:40 +0200

    code
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- FinnKino.js
+++ FinnKino.js
@@ -1,6 +1,31 @@
-function loadByTheatre(){
+function loadDoc() {
     var url = "https://www.finnkino.fi/xml/Schedule/";
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-}
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+  function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table="<tr><th>Title</th><th>LengthInMinutes</th><th>Rating</th><th>ProductionYear</th><th>Theatre</th></tr>";
+    var x = xmlDoc.getElementsByTagName("Show");
+    for (i = 0; i <x.length; i++) { 
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("LengthInMinutes")[0].childNodes[0].nodeValue +
+        "</td><td>" +   
+        x[i].getElementsByTagName("Rating")[0].childNodes[0].nodeValue +
+        "</td><td>" +     
+        x[i].getElementsByTagName("ProductionYear")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
+        "</td><td>";
+    }
+    document.getElementById("demo").innerHTML = table;
+  }
