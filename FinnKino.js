a98e52ab84d8af65dbf38dc06d9b25717f54bb65
Kearsz <16836684+Kearsz@users.noreply.github.com>
2021-11-20 12:13:51 +0200

    koodi valmis
    Signed-off-by: Kearsz <16836684+Kearsz@users.noreply.github.com>

--- FinnKino.js
+++ FinnKino.js
@@ -12,9 +12,10 @@ function loadDoc() {
   function myFunction(xml) {
     var i;
     var xmlDoc = xml.responseXML;
-    var table="<tr><th>Title</th><th>Length</th><th>Rating</th><th>Year</th><th>Theatre</th></tr>";
+    var table="<tr><th>Title</th><th>Length (Minutes)</th><th>Rating</th><th>Year</th><th>Theatre</th><th>Poster</th></tr>";
     var x = xmlDoc.getElementsByTagName("Show");
     for (i = 0; i <x.length; i++) { 
+      const imageUrl = x[i].getElementsByTagName('Images')[0].firstElementChild.textContent;
         table += "<tr><td>" +
         x[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
         "</td><td>" +
@@ -25,7 +26,9 @@ function loadDoc() {
         x[i].getElementsByTagName("ProductionYear")[0].childNodes[0].nodeValue +
         "</td><td>" +
         x[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
-        "</td><td>";
+        '</td><td><img src=' +
+        imageUrl +
+        '></td></tr>';
     }
     document.getElementById("demo").innerHTML = table;
   }
